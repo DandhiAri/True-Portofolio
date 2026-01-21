a2d0ea150111dea3fce4e534091fbd3670af1930
DandhiAri <dandhiari@gmail.com>
2026-01-21 22:41:20 +0700

    add quote section and add quote values on homepage for quote section

--- resources/js/pages/portofolio/homepage/quotesection.vue
+++ resources/js/pages/portofolio/homepage/quotesection.vue
@@ -1,25 +1,30 @@
 <template>
     <section class="flex justify-between">
-        <div>
-            「雨が降っても　花は咲くから　前を向いて」<br />
-            "Even if it rains, flowers will still bloom, so keep looking
-            forward."<br />
-            <p>-"lilac" by Mrs. Green Apple</p>
-        </div>
-        <div class="text-1xl max-w-3xs text-gray-500">
-            <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem
-                ipsum dolor, sit amet consectetur adipisicing elit.
-            </p>
-            <p class="text-black-800 bold text-3xl mt-2">More about me!</p>
+        <div v-for="quote in quotes" :key="quote.position" :class="quote.position === 'left' ? 'max-w-xs' : 'text-xs max-w-xs text-gray-600'">
+            <div v-if="quote.position === 'left'">
+                {{ quote.text }}<br />
+                {{ quote.translation }}<br />
+                <p>{{ quote.source }}</p>
+            </div>
+            <div v-else>
+                <p>{{ quote.text }}</p>
+                <p class="text-black font-bold text-lg mt-2">{{ quote.link }}</p>
+            </div>
         </div>
     </section>
 </template>
 <script setup lang="ts">
-    interface Props{
-        text:string;
-        translation:string;
-        source:string;
-    }
-    defineProps();
+interface Quote {
+    position: string;
+    text: string;
+    translation?: string;
+    source?: string;
+    link?: string;
+}
+
+interface Props {
+    quotes: Quote[];
+}
+
+defineProps<Props>();
 </script>
