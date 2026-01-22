53156bebe0783e1fcecc7007de35b1450680701e
DandhiAri <dandhiari@gmail.com>
2026-01-22 22:51:32 +0700

    add footer section on homepage, change element displaying full html symbols and text on footer, add CSS style on footer and add project section on component homepage file

--- resources/js/pages/portofolio/homepage/projectsection.vue
+++ resources/js/pages/portofolio/homepage/projectsection.vue
@@ -1,3 +1,29 @@
 <template>
-    
+    <section class="my-projects">
+        <div class="sub-section-title space-between">
+            My Work So far... and yet still continue!
+            <p class="right-quote">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos necessitatibus numquam quo sit distinctio
+                nisi, exercitationem officia.
+            </p>
+        </div>
+        <div class="project-body">
+            <div class="project-container">
+                <img class="img-project" src="img/projects/dummy.png" alt="project-img" srcset="">
+                <p>project name &#62;&#62;&#62;</p>
+            </div>
+            <div class="project-container">
+                <img class="img-project" src="img/projects/dummy.png" alt="project-img" srcset="">
+                <p>project name &#62;&#62;&#62;</p>
+            </div>
+            <div class="project-container">
+                <img class="img-project" src="img/projects/dummy.png" alt="project-img" srcset="">
+                <p>project name &#62;&#62;&#62;</p>
+            </div>
+            <div class="project-container">
+                <img class="img-project" src="img/projects/dummy.png" alt="project-img" srcset="">
+                <p>project name &#62;&#62;&#62;</p>
+            </div>
+        </div>
+    </section>
 </template>
