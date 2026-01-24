1ae75f53a49a6092e9f55a91c81a40d2498e591f
DandhiAri <dandhiari@gmail.com>
2026-01-24 22:21:54 +0700

    change variable name on homepage to contactinfo (was contact)

--- resources/js/pages/portofolio/homepage/contactsection.vue
+++ resources/js/pages/portofolio/homepage/contactsection.vue
@@ -2,8 +2,6 @@
     <div class="mt-10 grid grid-cols-[80%_20%]">
         <div class="text-4xl">
             <div v-html="text"></div>
-            <!-- Have a project idea? <strong>Let's Work Together!</strong><br>
-            Or maybe just saying hi&#8230;<span class="underline">Let's talk!</span> -->
             <hr class="mt-2">
         </div>
         <div class="m-1 row-span-2 rounded-md py-[3em] px-[1em] bg-blue-700 font-bold text-2xl text-white text-center ">
