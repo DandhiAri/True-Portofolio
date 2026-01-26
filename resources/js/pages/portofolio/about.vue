488ee91c1e2aeaf5e5f16ea69ea5e2b85b5b324e
DandhiAri <dandhiari@gmail.com>
2026-01-26 21:53:53 +0700

    remove elements from about page

--- resources/js/pages/portofolio/about.vue
+++ resources/js/pages/portofolio/about.vue
@@ -1,18 +1,2 @@
 <template>
-    <body>
-        <div class="img-container">
-            <img src="" alt="pfp">            
-        </div>
-        <div class="profile-container">
-            <p>Nama: Dandhi Dwi Ari Prasetya</p>
-            <p>Domisili : Jember</p>
-            <p>HIII</p>
-        </div>
-    </body>
 </template>
-<style>
-    body{
-        display: flex;
-        justify-content: space-between;
-    }
-</style>
