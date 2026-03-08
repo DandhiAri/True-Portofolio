00c1da2dea2f29e8acc9872852df8cf2b4a25416
DandhiAri <dandhiari@gmail.com>
2026-03-08 17:28:34 +0700

    feat: add copy email to clipboard in contactinfo section and change email value on secondpage

--- resources/js/pages/portofolio/homepage/contactinfo.vue
+++ resources/js/pages/portofolio/homepage/contactinfo.vue
@@ -1,13 +1,25 @@
 <template>
     <div class="bg-neutral-900 p-5 grid content-center text-white text-center h-15">
-        <p class="flex justify-center"> <div class="underline mr-1"> Email:  {{email}} </div> | <div v-show="phone"> Phone: {{phone}} |</div><a class="underline ml-1" href="{{ github }}">Github: @DandhiAri</a></p>
+        <p class="flex justify-center"> 
+            <div class="cursor-pointer underline mr-1" @click="copyURL"> Email: dandhiari (at) gmail [dot] com </div> | 
+            <div v-show="phone"> Phone: {{phone}} } | </div> 
+            <a class="underline ml-1" href="{{ github }}">Github: @DandhiAri</a>
+        </p>
     </div>
 </template>
-<script setup lang="ts">
-    interface Props{
-        email:string;
-        phone?:string;   
-        github:string;
-    }
-    defineProps<Props>();
+<script setup >
+const props = defineProps({
+    email: String,
+    // phone: String,
+    github: String
+});
+const copyURL = () => {
+    navigator.clipboard.writeText(props.email)
+        .then(() => {
+            alert('Email address copied to clipboard!');
+        })
+        .catch(err => {
+            console.error('Failed to copy email address: ', err);
+        });
+};
 </script>
--- resources/js/pages/secondpage.vue
+++ resources/js/pages/secondpage.vue
@@ -1,34 +1,20 @@
 <template>
-    <!-- <Herobanner :name="portofolioData.name" :title="portofolioData.title" /> -->
+    <Herobanner :name="portofolioData.name" :title="portofolioData.title" />
     <Contactinfo :email="portofolioData.contactinfo.email" />
     <Projectsection :title="portofolioData.project.title" :quote="portofolioData.project.quote" :projects="portofolioData.project.projects"/>
     <!-- <Contactsection :quotes="portofolioData.quotes" /> -->
 </template>
 <script setup>
-    import Contactinfo from './portofolio/homepage/contactinfo.vue';
-    import Contactsection from './portofolio/homepage/contactsection.vue';
     import Herobanner from './portofolio/homepage/herobanner.vue';
+    import Contactinfo from './portofolio/homepage/contactinfo.vue';
     import Projectsection from './portofolio/homepage/projectsection.vue';
 
     const portofolioData = {
         name: 'Dandhi Dwi Ari Prasetya',
         title: 'WEB DEVELOPER',
         contactinfo: {
-            email: 'dandhiari (at) gmail [dot] com'
+            email: 'dandhiari@gmail.com '
         },
-        quotes: [
-            {
-                position: 'left',
-                text: '「雨が降っても　花は咲くから　前を向いて」',
-                translation: '"Even if it rains, flowers will still bloom, so keep looking forward."',
-                source: '-"lilac" by Mrs. Green Apple'
-            },
-            {
-                position: 'right',
-                text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
-                link: 'More about me!'
-            }
-        ],
         project:{
             title:"My Work So far... and yet still continue!",
             quote:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos necessitatibus numquam quo sit distinctio nisi, exercitationem officia.",
