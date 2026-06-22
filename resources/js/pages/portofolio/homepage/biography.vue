5c59d0a171abac38cbf52e3f9c2cf8c60a2561c5
DandhiAri <dandhiari@gmail.com>
2026-06-22 23:55:26 +0700

    add biography section, add content and style to biography section

---
+++ resources/js/pages/portofolio/homepage/biography.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="bg-black relative p-10">
+        <p class="text-lg text-gray-300 leading-relaxed">{{ content }}</p>
+        <div class="flex flex-wrap justify-center gap-3 mt-10 max-w-10xl mx-auto">
+            <div 
+                v-for="tool in tools" 
+                :key="tool.name" 
+                class="transition-all duration-200 hover:-translate-y-0.5"
+            >
+                <span class="block rounded-full border border-zinc-800/80 bg-zinc-900/30 px-5 py-2 text-sm font-medium text-zinc-300 shadow-sm backdrop-blur-sm select-none">
+                    {{ tool.name }}
+                </span>
+            </div>
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+    interface Tool {
+        name: string;
+        icon: string;
+    }
+
+    interface Props {
+        content: string;
+        tools: Tool[];
+    }
+
+    defineProps<Props>();
+</script>
--- resources/js/pages/secondpage.vue
+++ resources/js/pages/secondpage.vue
@@ -3,11 +3,10 @@
         <div class="relative">
             <Herobanner :name="portofolioData.name" :title="portofolioData.title" />
         </div>
-
         <Contactinfo :email="portofolioData.contactinfo.email" :github="portofolioData.contactinfo.github" />
+        <Biography :content="portofolioData.biography.content"  :tools="portofolioData.biography.tools"/>
         <section class="relative p-10">
-            <!-- <div class="absolute inset-y-0 left-1/2 z-0 w-[20px] -translate-x-1/2 bg-zinc-800 pointer-events-none"></div> -->
-            <div class="absolute -inset-1 bg-repeat-round opacity-30 bg-[url(/img/4ktopo.jpg)]" ></div>
+            <div class="absolute -inset-1 bg-repeat-round opacity-30 bg-[url(/img/4ktopo.jpg)]"></div>
 
             <div class="absolute -inset-1 bg-gradient-to-b from-black via-black/50 to-transparent pointer-events-none"></div>
             <Projectsection class="relative z-999" :title="portofolioData.projects.title" :quote="portofolioData.projects.quote" :projects="portofolioData.projects.project"/>
@@ -18,6 +17,7 @@
 <script setup>
     import Herobanner from './portofolio/homepage/herobanner.vue';
     import Contactinfo from './portofolio/homepage/contactinfo.vue';
+    import Biography from './portofolio/homepage/biography.vue';
     import Projectsection from './portofolio/homepage/projectsection.vue';
     import Footer from './portofolio/homepage/footer.vue';
 
@@ -28,8 +28,36 @@
             email: 'dandhiari@gmail.com',
             github: 'https://github.com/DandhiAri'
         },
+        biography: {
+            content: "Hello, I'm Dandhi Dwi Ari Prasetya, a passionate web developer and network engineer. I specialize in creating dynamic and responsive web applications, as well as designing and managing complex network infrastructures. With a strong foundation in both front-end and back-end development, I strive to deliver seamless user experiences and robust solutions. My expertise extends to IoT engineering, where I explore innovative ways to connect devices and enhance automation. I am committed to continuous learning and staying updated with the latest technologies to provide cutting-edge solutions.",
+            tools: [
+                { name: 'HTML', icon: 'img/tools/html.png' },
+                { name: 'CSS', icon: 'img/tools/css.png' },
+                { name: 'JavaScript', icon: 'img/tools/javascript.png' },
+                { name: 'Vue.js', icon: 'img/tools/vue.png' },
+                { name: 'Laravel', icon: 'img/tools/laravel.png' },
+                { name: 'Node.js', icon: 'img/tools/nodejs.png' },
+                { name: 'Python', icon: 'img/tools/python.png' },
+                { name: 'Docker', icon: 'img/tools/docker.png' },
+                { name: 'Git', icon: 'img/tools/git.png' },
+                { name: 'HTML', icon: 'img/tools/html.png' },
+                { name: 'CSS', icon: 'img/tools/css.png' },
+                { name: 'JavaScript', icon: 'img/tools/javascript.png' },
+                { name: 'Vue.js', icon: 'img/tools/vue.png' },
+                { name: 'Laravel', icon: 'img/tools/laravel.png' },
+                { name: 'Node.js', icon: 'img/tools/nodejs.png' },
+                { name: 'Python', icon: 'img/tools/python.png' },
+                { name: 'Docker', icon: 'img/tools/docker.png' },
+                { name: 'Git', icon: 'img/tools/git.png' },
+                { name: 'Laravel', icon: 'img/tools/laravel.png' },
+                { name: 'Node.js', icon: 'img/tools/nodejs.png' },
+                { name: 'Python', icon: 'img/tools/python.png' },
+                { name: 'Docker', icon: 'img/tools/docker.png' },
+                { name: 'Git', icon: 'img/tools/git.png' },
+            ]
+        },
         projects:{
-            title:"My Work So Far... and Yet Still CONTINUE!",
+            title:"Projects",
             // quote:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos necessitatibus numquam quo sit distinctio nisi, exercitationem officia.",
             project:[
                 {
